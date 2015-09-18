9d52ee1da09a9ac691c029e4e6a885cbaaa4d0d4
Gabe Fernando <gef756@gmail.com>
2015-09-18 18:43:28 -0400

    DOC: add reference solution for duplexer redux

--- 05-stream-adventure/12-duplexer-redux.js
+++ 05-stream-adventure/12-duplexer-redux.js
@@ -22,3 +22,25 @@ module.exports = function (counter) {
 
   return duplexer2(input, counter);
 }
+
+
+
+// Here's the reference solution:
+
+  // var duplexer = require('duplexer2');
+  // var through = require('through2').obj;
+  //
+  // module.exports = function (counter) {
+  //     var counts = {};
+  //     var input = through(write, end);
+  //     return duplexer(input, counter);
+  //
+  //     function write (row, _, next) {
+  //         counts[row.country] = (counts[row.country] || 0) + 1;
+  //         next();
+  //     }
+  //     function end (done) {
+  //         counter.setCounts(counts);
+  //         done();
+  //     }
+  // };
