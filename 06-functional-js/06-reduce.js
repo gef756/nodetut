70bdcb5fbd5591baf8471629b1c358640523760c
Gabe Fernando <gef756@gmail.com>
2015-09-21 19:46:56 -0400

    ENH: add reference solution

--- 06-functional-js/06-reduce.js
+++ 06-functional-js/06-reduce.js
@@ -1,7 +1,14 @@
 module.exports = function (inputWords) {
   var wordCounts = {}
   inputWords.map(function (word) {
-    return wordCounts[word] = (wordCounts[word] || 0) + 1; 
+    return wordCounts[word] = (wordCounts[word] || 0) + 1;
   });
   return wordCounts
+}
+
+function countWords2(inputWords) {
+  return inputWords.reduce(function(countMap, word) {
+    countMap[word] = ++countMap[word] || 1;
+    return countMap;
+  }, {});
 }
