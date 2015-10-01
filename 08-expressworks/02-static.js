b3cee69e39334a8a3f20f5d180241eb69e7b3039
Gabe Fernando <gef756@gmail.com>
2015-10-01 19:05:06 -0400

    DOC: add some comments

--- 08-expressworks/02-static.js
+++ 08-expressworks/02-static.js
@@ -2,6 +2,8 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+// Serve a static page provided as an arg, or use what's in the public folder.
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
+// Port number for listening supplied as arg.
 app.listen(process.argv[2]);
