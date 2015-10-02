679d5b7398cd0ae27336a4055c82bc35a274903e
Gabe Fernando <gef756@gmail.com>
2015-10-02 07:43:44 -0400

    DOC: add alternate solution using middleware

--- 08-expressworks/06-param-pam.js
+++ 08-expressworks/06-param-pam.js
@@ -15,5 +15,16 @@ app.put('/message/:id', function(req, res) {
   res.end(calcHash(req.params.id));
 })
 
+// Alternatively use middleware:
+// app.param('id', function (req, res, next, id) {
+//   req.id = id;
+//   next();
+// })
+//
+// app.put('/message/:id', function (req, res, next) {
+//   res.send(calcHash(req.id));
+//   next();
+// })
+
 // Start server and listen
 app.listen(process.argv[2]);
